2b8f5082ba5c1fae5b0194acba28e06832391dea
zeitmeister <simon.sporrong@hotmail.se>
2019-10-14 10:41:32 +0200

    initial commit

---
+++ BookService/CsvParser.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookService
+{
+    /// <summary>
+    /// Parses books and authors from a csv-file with a specific format.
+    /// </summary>
+    class CsvParser
+    {
+        private List<Book> Books = new List<Book>();
+        private Dictionary<string, Author> Authors =
+            new Dictionary<string, Author>();
+
+        /// <summary>
+        /// Get all parsed books.
+        /// Call ParseCsv before calling this method.
+        /// </summary>
+        /// <returns>A set of books.</returns>
+        public IEnumerable<Book> GetBooks()
+        {
+            return Books;
+        }
+
+        /// <summary>
+        /// Get all parsed authors.
+        /// Call ParseCsv before calling this method.
+        /// </summary>
+        /// <returns>A set of authors.</returns>
+        public IEnumerable<Author> GetAuthors()
+        {
+            return Authors.Values;
+        }
+
+        /// <summary>
+        /// Parses a csv-file at path:"/../../files/books.csv"
+        /// containing books and their authors.
+        /// </summary>
+        public void ParseCsv()
+        {
+            var path = Application.StartupPath + "/../../Files/books.csv";
+            var lines = File.ReadAllLines(path, Encoding.GetEncoding("iso-8859-1"));
+
+            foreach (string line in lines)
+            {
+                var items = line.Split(new string[] { ";" }, StringSplitOptions.None);
+
+                int year, votes;
+                int.TryParse(items[2], out year);
+                int.TryParse(items[5], out votes);
+
+                double rating;
+                var style = NumberStyles.Number;
+                var culture = CultureInfo.CreateSpecificCulture("en-US");
+                double.TryParse(items[4], style, culture, out rating);
+
+
+                Book book = new Book
+                {
+                    ISBN = items[0],
+                    Title = items[3],
+                    YearOfPublication = year,
+                    Rating = rating,
+                    NumberOfUserVotes = votes,
+                    Authors = new List<Author>()
+                };
+
+                Books.Add(book);
+
+                var authors = items[1].Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var c in authors)
+                {
+                    Author author;
+
+                    if (!Authors.ContainsKey(c))
+                    {
+                        author = new Author
+                        {
+                            Books = new List<Book>(),
+                            Name = c
+                        };
+
+                        Authors.Add(c, author);
+                    }
+                    else
+                        author = Authors[c];
+
+                    author.Books.Add(book);
+                    book.Authors.Add(author);
+                }
+            }
+        }
+    }
+}
---
+++ BookService/SimpleDI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookService
+{
+    class SimpleDI
+    {
+        /// <summary>
+        /// Name of the registered service.
+        /// </summary>
+        private static string serviceName;
+
+        /// <summary>
+        /// Constructor that registers a service from the configuration file.
+        /// </summary>
+        static SimpleDI()
+        {
+            RegisterService(BookService.Configuration.IBookService);
+        }
+
+        /// <summary>
+        /// Register the name of the service
+        /// </summary>
+        /// <param name="name">name.</param>
+        public static void RegisterService(string name)
+        {
+            serviceName = name;
+        }
+
+        /// <summary>
+        /// Gets an instance of the registered service.
+        /// </summary>
+        /// <returns></returns>
+        public static IBookService GetService()
+        {
+            if (serviceName == "InMemoryBookService")
+                return new InMemoryBookService();
+            if (serviceName == "CsvBookService")
+                return new CsvBookService();
+
+            throw new InvalidOperationException("No matching service!");
+        }
+    }
+}
